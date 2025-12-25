78c2eb081a28540c8b01a83c880b811f3fa2d630
AndrewNG <nguyenquyenforwork@gmail.com>
2025-12-25 11:10:42 +0700

    ...

--- Application/DTOs/Requests/CreateProductVariationRequest.cs
+++ Application/DTOs/Requests/CreateProductVariationRequest.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,8 +9,12 @@ namespace Application.DTOs.Requests
 {
     public class CreateProductVariationRequest
     {
+        [Required]
         public string ProductId { get; set; } = null!;
+
+        [Required]
         public decimal Price { get; set; }
+
         public int StockQuantity { get; set; }
 
         public List<CreateVariationOptionRequest> Options { get; set; } = new();
